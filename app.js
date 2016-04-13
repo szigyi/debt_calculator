51131c5bfe3eb7aa33bcba11a2e2d7888fda75f1
Szabolcs Szilagyi <szabolcs.szilagyi.hu@gmail.com>
2016-04-13 21:56:51 +0200

    New feature to handle input form

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var numeral = require('numeral');
 var express = require('express');
 var moment = require('moment');
 var swig  = require('swig');
+var bodyParser = require('body-parser');
 var app = express();
 
 var port = process.env.PORT || 8080;
@@ -9,21 +10,25 @@ var port = process.env.PORT || 8080;
 // This is where all the magic happens!
 app.engine('html', swig.renderFile);
 
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 app.set('view engine', 'html');
 app.set('views', __dirname + '/');
 
 numeral.defaultFormat('0,0 HUF');
 
-var response = {};
-response.initialDebt = 2000000;
-response.initialDays = 730;
-response.endDate = new Date(2016, 11, 01, 01, 01, 01, 1);
-response.endDateMillis = +new Date(response.endDate);
+function debtCalculator(initialDebt, initialDays, endDate, actualDate) {
+	var response = {};
+	response.initialDebt = initialDebt;
+	response.initialDays = initialDays;
+	response.endDate = endDate;
+	response.endDateMillis = +new Date(response.endDate);
 
-response.endDateFormatted = new moment(response.endDate).format("MMM Do YYYY");
+	response.endDateFormatted = new moment(response.endDate).format("MMM Do YYYY");
 
-app.get('/', function (req, res) {
-	response.actualDate = new Date();
+	response.actualDate = actualDate;
 	response.actualDateMillis = +new Date(response.actualDate);
 	var sub = response.endDateMillis - response.actualDateMillis;
 	var diff = new moment.duration(sub);
@@ -36,6 +41,21 @@ app.get('/', function (req, res) {
 	response.debtLeftFormatted = numeral(response.debtLeft).format();
 	response.daysLeftFormatted = Math.floor(response.daysLeft);
 
+	return response;
+}
+
+app.get('/', function (req, res) {
+	var response = debtCalculator(2000000, 730, new Date(2016, 11, 01, 01, 01, 01, 1), new Date());
+	res.render('index', response);
+});
+
+app.post('/', function (req, res) {
+	var initialDebt = req.body.initial_debt;
+	var initialDays = req.body.initial_days;
+	var endDate = req.body.end_date;
+	var actualDate = req.body.actual_date;
+	var response = debtCalculator(initialDebt, initialDays, new Date(endDate), new Date(actualDate));
+	console.log(response);
 	res.render('index', response);
 });
 
